fdef06955d738fd848e4e2c3ac8cd1ddb5edff2d
keito58-tk <keito1904@outlook.jp>
2024-05-03 17:27:19 +0900

    commit

--- java_study/src/kadai_008/Price_Chapter08.java
+++ java_study/src/kadai_008/Price_Chapter08.java
@@ -7,12 +7,12 @@ public class Price_Chapter08 {
 		int serviceCost ;
 		
 		serviceCost = switch (userAge) {
-			case 10 -> 1000;
-			case 20 -> 2000;
-			case 30, 40 -> 3000;
+			case 10         -> 1000;
+			case 20         -> 2000;
+			case 30, 40     -> 3000;
 			case 50, 60, 70 -> 4000;
-			case 80 -> 5000;
-			default -> 500;
+			case 80         -> 5000;
+			default         ->  500;
 		};
 		
 		System.out.println(userAge + "代の料金は" + serviceCost + "円");
